c2dfd22055b222807c918bcaf38a08915891eb69
Sameh Elmeligy <109442419+SamehElsaid@users.noreply.github.com>
2026-02-18 11:00:55 +0200

    Enhance menu fetching by using subdomain from request headers

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -3,6 +3,7 @@ import UseDispatchMenu from "@/hooks/UseDispatchMenu";
 import { axiosGet } from "@/shared/axiosCall";
 import { MenuItem, MenuInfo, MenuCustomizations, Category } from "@/types/menu";
 import { Ad } from "@/types/Ad";
+import { headers } from "next/headers";
 
 type MenuResponse = {
   menu: MenuInfo;
@@ -12,17 +13,30 @@ type MenuResponse = {
   categories: Category[];
 };
 
+
 const getMenu = async (locale: string) => {
+  const headersList = await headers();
+  const host = headersList.get("host") || "";
+
+
+  // memos.ensmenu.com
+  const subdomain = host.split(".")[0];
+
   const slug = process.env.NEXT_PUBLIC_Dev
     ? process.env.NEXT_PUBLIC_SUB_DOMAIN
-    : "menu-ar";
-  const response = await axiosGet<{ data: MenuResponse }>(`/public/menu/${slug}`, locale)
+    : subdomain;
+
+  const response = await axiosGet<{ data: MenuResponse }>(
+    `/public/menu/${slug}`,
+    locale
+  );
 
   if (response.status) {
     return response?.data?.data;
   }
   return null;
 };
+
 
 export default async function MainLayout({
   children,
